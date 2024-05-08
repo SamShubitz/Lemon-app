43ec7b80ec33b84b0de9e2a442b2e03bfce6c6ef
Sam Shubitz <shubitzs@gmail.com>
2024-05-08 15:28:38 -0400

    Added meta tags, html, header and footer component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,54 @@
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <p>Homepage</p>
-    </div>
+    <>
+      <Header/>
+      <main>
+        <h1>Little Lemon</h1>
+        <h2>Chicago</h2>
+        <article>
+          <p>We are a family owned Mediterranean
+          restaurant, focused on traditional
+          recipes served with a modern twist.</p>
+          <button>Reserve a table</button>
+          <img src="/restauranfood.jpg" alt="promo plate" width="300"/>
+        </article>
+        <h2>Specials</h2>
+        <article>
+          <section>
+            <h4>Greek Salad</h4>
+            <p>$12.99</p>
+            <p>The famous greek salad of crispy
+              lettuce, peppers, olives and our
+              Chicago style feta cheese, garnished
+              with crunchy garlic and rosemary croutons. </p>
+            <b>Order a delivery</b>
+          </section>
+        <h4>Bruschetta</h4>
+          <section>
+            <p>$12.99</p>
+            <p>The famous greek salad of crispy
+              lettuce, peppers, olives and our
+              Chicago style feta cheese, garnished
+              with crunchy garlic and rosemary croutons. </p>
+            <b>Order a delivery</b>
+          </section>
+          <section>
+            <h4>Lemon Dessert</h4>
+            <p>$12.99</p>
+            <p>The famous greek salad of crispy
+              lettuce, peppers, olives and our
+              Chicago style feta cheese, garnished
+              with crunchy garlic and rosemary croutons. </p>
+            <b>Order a delivery</b>
+          </section>
+        </article>
+      </main>
+      <Footer/>
+    </>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+export default function Footer() {
+    return (
+        <footer>
+        <img src="/Logo.svg" alt="logo"></img>
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Menu</li>
+            <li>Reservations</li>
+            <li>Order online</li>
+            <li>Log in</li>
+          </ul>
+        </nav>
+      </footer>
+    );
+}
